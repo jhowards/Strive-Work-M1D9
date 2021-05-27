7fa3c09794843dc66c791caef6a5dae9c0f7f32f
jhowards <github@jameshoward.uk>
2021-05-27 20:15:01 +0100

    Random unique numbers

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+
 window.onload = function(){
     displayBingoBoard()
-    const range = fillArray();
-    generateRandNumber(range)
+    const randomButton = document.getElementById("randomButton");
+    const array = createArray()
+    const randomArray = shuffleArray(array)
+    console.log("loaded")
+
+    randomButton.addEventListener("click", function () {
+        numberSelect(randomArray);
+      });
 }
 
 
@@ -21,19 +28,56 @@ const displayBingoBoard = function(){
     bingoBoard.appendChild(number)
 }
 
-const numberSelect = function(){
-random = Math.floor(Math.random() * 76) + 1
+
+
+//   const getRandomNum = function(range){
+//       console.log(range)
+//     const randIndex = Math.floor(Math.random() * 76) + 1
+//     const random = range.splice(randIndex, 1)[0]
+//     return random
+//   }
+
+const numberSelect = function(randomArray){
+    
+    // const random = Math.floor(Math.random() * 76) + 1
 const numbers = document.querySelectorAll(".number")
 let topNumber = document.getElementById("randomNumber")
 
 
-for (let i = 0; i <= 75; i++){
-    if(i === random){
-            topNumber.innerHTML = random
+ for (let i = 0; i <= 76; i++){
+    
+     if(i === randomArray[0]){
+            topNumber.innerHTML = randomArray[0]
             numbers[i - 1].classList.add("chosen")
-            console.log(random)
-        
+            console.log(randomArray[0])
+            randomArray.shift()
+            return
+        }
+ }
+}
+
+// let randoms = new Set();
+// while(randoms.size<76){
+//   randoms.add(1 + Math.floor(Math.random() * 76));
+// }
+// console.log([...randoms.values()]);
+
+const createArray = function () {
+    const arr = [];
+    for (let i = 0; i < 76; i++) {
+      arr.push(i + 1);
     }
+    return arr;
+  };
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
 }
-}
+
 
